542b2941441955e81dfb079c887bc7bf78830001
cowhi <github.rg@gmail.com>
2013-07-28 21:41:37 +0000

    Second commit

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Blabla!');
+  response.send('Hello Blabla Bla Bla Bla');
 });
 
 var port = process.env.PORT || 5000;
