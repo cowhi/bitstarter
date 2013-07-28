aef7a799bac48d2c76184d163afffdac4c3df717
cowhi <github.rg@gmail.com>
2013-07-28 22:30:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var port = process.env.PORT || 5000;
+var data = new Buffer(fs.readFileSync('index.html'));
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World');
+  response.send(data.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
